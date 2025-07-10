81a183daf292977748571dcd0d1e4a6aa6f962ac
amir <cavalier7654@gmail.com>
2025-07-10 23:00:37 +0330

    test env file - The env file is a security file and should not be published anywhere, but this is just for testing.

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module practice
 
 go 1.24
+
+require github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
 
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	pass := os.Getenv("PASSWORD")
+	fmt.Println(pass)
 }
